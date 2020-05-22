a161e96b6bdb4dd8353a355b3fe4d37c0f9f1610
Ivan <i.s.m.mipt@yandex.ru>
2020-05-22 22:07:04 +0300

    Добавлена заготовка класса Market в проект trader.

---
+++ projects/trader/source/market/market.cpp
@@ -0,0 +1,120 @@
+#include "market.hpp"
+
+namespace solution
+{
+	namespace trader
+	{
+		using Severity = shared::Logger::Severity;
+
+		Market::path_t Market::get(const std::string & asset, const std::string & scale, time_point_t first, time_point_t last) const
+		{
+			RUN_LOGGER(logger);
+
+			try
+			{
+				path_t path = asset + "_" + scale + Extension::txt;
+
+				shared::Python_GIL lock;
+
+				try
+				{
+					boost::python::exec("from market import get", 
+						shared::Python::global(), shared::Python::global());
+
+					shared::Python::global()["get"]
+						(asset.c_str(), scale.c_str(), make_date(first).c_str(), make_date(last).c_str(), path.string().c_str());
+				}
+				catch (const boost::python::error_already_set &)
+				{
+					logger.write(Severity::error, shared::Python::exception());
+				}
+				catch (const std::exception & exception)
+				{
+					shared::catch_handler < market_exception > (logger, exception);
+				}
+
+				return path;
+			}
+			catch (const std::exception & exception)
+			{
+				shared::catch_handler < market_exception > (logger, exception);
+			}
+		}
+		/*
+		Market::path_t Market::get(const std::string & asset, const std::string & scale, std::size_t quantity) const
+		{
+			RUN_LOGGER(logger);
+
+			try
+			{
+				using shared_memory_t = boost::interprocess::managed_shared_memory;
+
+				using record_t = 
+					boost::interprocess::basic_string < char, std::char_traits < char >,
+					    boost::interprocess::allocator < char, shared_memory_t::segment_manager > > ;
+				
+				using record_allocator_t =
+				    boost::interprocess::allocator < record_t, shared_memory_t::segment_manager > ;
+
+				using deque_t = boost::interprocess::deque < record_t, record_allocator_t > ;
+
+				using mutex_t = boost::interprocess::interprocess_mutex;
+
+				const auto shared_memory_name = "QUIK_" + asset + "_" + scale;
+
+				shared_memory_t shared_memory(boost::interprocess::open_only, shared_memory_name.c_str());
+
+				auto deque = shared_memory.find < deque_t > (boost::interprocess::unique_instance).first;
+				auto mutex = shared_memory.find < mutex_t > (boost::interprocess::unique_instance).first;
+				
+				auto path = directory; path /= asset + "_" + scale + Extension::txt;
+
+				{
+					boost::interprocess::scoped_lock < mutex_t > lock(*mutex);
+
+					std::fstream fout(path.string(), std::ios::out);
+
+					if (!fout)
+					{
+						throw market_exception("cannot open file " + path.string());
+					}
+
+					fout << "date,time,open,high,low,close,v" << std::endl;
+
+					for (const auto & record : *deque)
+					{
+						fout << record << std::endl;
+					}
+				}
+
+				return path;
+			}
+			catch (const std::exception & exception)
+			{
+				shared::catch_handler < market_exception > (logger, exception);
+			}
+		}
+		*/
+		Market::date_t Market::make_date(time_point_t time_point) const
+		{
+			RUN_LOGGER(logger);
+
+			try
+			{
+				auto time = clock_t::to_time_t(time_point);
+
+				std::stringstream sout;
+
+				sout << std::put_time(std::localtime(&time), "%y%m%d");
+
+				return sout.str();
+			}
+			catch (const std::exception & exception)
+			{
+				shared::catch_handler < market_exception > (logger, exception);
+			}
+		}
+
+	} // namespace trader
+
+} // namespace solution
---
+++ projects/trader/source/market/market.hpp
@@ -0,0 +1,107 @@
+#ifndef SOLUTION_TRADER_MARKET_HPP
+#define SOLUTION_TRADER_MARKET_HPP
+
+#include <boost/config.hpp>
+
+#ifdef BOOST_HAS_PRAGMA_ONCE
+#  pragma once
+#endif // #ifdef BOOST_HAS_PRAGMA_ONCE
+
+#include <chrono>
+#include <cstdint>
+#include <ctime>
+#include <exception>
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+#include <stdexcept>
+#include <string>
+#include <sstream>
+
+#define BOOST_PYTHON_STATIC_LIB
+
+//#include <boost/interprocess/allocators/allocator.hpp>
+//#include <boost/interprocess/containers/deque.hpp>
+//#include <boost/interprocess/containers/string.hpp>
+//#include <boost/interprocess/managed_shared_memory.hpp>
+//#include <boost/interprocess/sync/interprocess_mutex.hpp>
+//#include <boost/interprocess/sync/named_mutex.hpp>
+//#include <boost/interprocess/sync/scoped_lock.hpp>
+//#include <boost/interprocess/managed_windows_shared_memory.hpp>
+#include <boost/python.hpp>
+
+#include "../../../shared/source/logger/logger.hpp"
+#include "../../../shared/source/python/python.hpp"
+
+namespace solution
+{
+	namespace trader
+	{
+		class market_exception : public std::exception
+		{
+		public:
+
+			explicit market_exception(const std::string & message) noexcept :
+				std::exception(message.c_str())
+			{}
+
+			explicit market_exception(const char * const message) noexcept :
+				std::exception(message)
+			{}
+
+			~market_exception() noexcept = default;
+		};
+
+		class Market
+		{
+		public:
+
+			using clock_t = std::chrono::system_clock;
+
+			using path_t = std::filesystem::path;
+
+		private:
+
+			using time_point_t = std::chrono::time_point < clock_t > ;
+
+			using date_t = std::string;
+
+		private:
+
+			struct Extension
+			{
+				using extension_t = std::string;
+
+				static inline const extension_t txt = ".txt";
+				static inline const extension_t csv = ".csv";
+				static inline const extension_t dat = ".dat";
+
+				static inline const extension_t empty = "";
+			};
+
+		public:
+
+			Market() noexcept = default;
+
+			~Market() noexcept = default;
+
+		public:
+
+			path_t get(const std::string & asset, const std::string & scale, time_point_t first, time_point_t last) const;
+			
+			// path_t get(const std::string & asset, const std::string & scale, std::size_t quantity) const;
+			
+		private:
+
+			date_t make_date(time_point_t time_point) const;
+			
+		private:
+
+			static inline const path_t directory = "market/candles";
+		};
+
+	} // namespace trader
+
+} // namespace solution
+
+#endif // #ifndef SOLUTION_TRADER_MARKET_HPP
